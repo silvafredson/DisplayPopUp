d49b8a532bb838cbe191ff439100bd7b5c2f0152
Fredson Silva <silva.fredson@hotmail.com>
2022-10-24 15:58:48 -0300

    Initial Commit

---
+++ DisplayPopUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DisplayPopUp
+//
+//  Created by Fredson Silva on 24/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DisplayPopUp/DisplayPopUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayPopUpApp.swift
+//  DisplayPopUp
+//
+//  Created by Fredson Silva on 24/10/22.
+//
+
+import SwiftUI
+
+@main
+struct DisplayPopUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
